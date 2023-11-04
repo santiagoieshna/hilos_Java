f166426a7cc7fc12e220f6a9b4af39d55306ee44
xtalo <117736097+santiagoieshna@users.noreply.github.com>
2023-11-04 01:48:51 +0100

    init Concurrencias

---
+++ Concurrencia01/src/crearThread01/way1.java
@@ -0,0 +1,10 @@
+package crearThread01;
+
+public class way1 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ Concurrencia01/src/crearThread02/way2.java
@@ -0,0 +1,10 @@
+package crearThread02;
+
+public class way2 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
